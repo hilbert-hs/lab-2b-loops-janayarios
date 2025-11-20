6e1a26014f255372391937ff080b32facf7c950d
janayarios <jesus_anaya57@icloud.com>
2025-11-20 17:11:08 +0000

    finish Grades

--- Grades.java
+++ Grades.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
-  //  write your code here
-  
+  public class Grades {
+
+  public static void main(String[] args) {
+    //  write your code here
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How many grades do you have?");
+    int numGrades = scan.nextInt();
+    double gradeAvg = 0;
+
+    for (int i = 0; i < numGrades; i++) {
+
+      System.out.println("Type in your grade");
+      double grade = scan.nextDouble();
+      gradeAvg = gradeAvg + grade;
+
+    }
+
+    System.out.println((double) gradeAvg / (double) numGrades);
+    
+  } 
 }
 
 // write any helper methods here
--- Tickets.java
+++ Tickets.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
+  public class Tickets {
 public static void main(String[] args) {
   // write your code here
 
+}
 }
 
 // write any helper methods here
